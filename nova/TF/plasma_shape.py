2addfc63c005e9ec79d6c8d76116637fa95f5b6d
Simon <twistersi@gmail.com>
2017-02-17 18:53:41 +0000

    minor update

--- nova/TF/plasma_shape.py
+++ nova/TF/plasma_shape.py
@@ -46,11 +46,11 @@ demo.plot_limiter()
 pl.axis('off')
 #tf.fill()
 
-sf.cpasma *= 1.0
-eq = EQ(sf,pf,dCoil=1.5,sigma=0,boundary=sf.get_sep(expand=1.5),n=5e3) 
+
+eq = EQ(sf,pf,dCoil=1.5,sigma=0,boundary=sf.get_sep(expand=1.2),n=2e3) 
 eq.gen_opp()
 
-sf.contour()
+#sf.contour()
 
 #eq.plotb()
 
@@ -62,27 +62,38 @@ L = inv.grid_coils(offset=0.3)
 
 inv.fix_boundary_psi(N=25,alpha=1-1e-4,factor=1)  # add boundary points
 #inv.fix_boundary_feild(N=25,alpha=1-1e-4,factor=1)  # add boundary points
-inv.add_null(factor=1,point=sf.Xpoint)
+#inv.add_null(factor=1,point=sf.Xpoint)
         
 
+sf.cpasma *= 0.99
+eq.gen_opp()
+inv.update_coils()
+pf.plot(coils=eq.coil,label=False,plasma=True,current=False)
 
-inv.set_swing()
+inv.set_swing(width=10)
 inv.update_limits(LCS=[-9.5,9.5])
 
 inv.initialize_log()
 inv.set_background()
-inv.get_weight()
-inv.set_Lo(L)  # set position bounds
-Lnorm = loops.normalize_variables(inv.Lo)
-inv.update_position(Lnorm,update_area=True)
+#inv.get_weight()
 
+#inv.set_Lo(L)  # set position bounds
+#Lnorm = loops.normalize_variables(inv.Lo)
+#inv.update_position(Lnorm,update_area=True)
+
+inv.ff.set_force_feild(state='active')  
+inv.set_foreground()
+
+inv.swing_flux()
 eq.gen_opp()
 
-#pf.plot(coils=pf.coil,label=False,current=True) 
+pf.plot(coils=pf.coil,label=True,current=True) 
 pf.plot(coils=eq.coil,label=False,plasma=True,current=False) 
 sf.contour(boundary=True)
 inv.plot_fix(tails=True)
 
 inv.ff.plot(scale=1.5)
+
+loops.plot_variables(inv.Io,scale=1,postfix='MA')
 
 #sf.eqwrite(pf,config=config['eq'])

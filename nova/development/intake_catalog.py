e52c10de3cebf065aaa71e7967cc76e34a6a2cd8
Mcintosh Simon <mcintos@iter.org>
2020-09-27 21:01:56 +0200

    Intake test cases

--- nova/development/intake_catalog.py
+++ nova/development/intake_catalog.py
@@ -11,7 +11,7 @@ from nova.utilities.IO import pythonIO
 #                           LocalCatalogEntry('name', 'description', 'csv')})
 
 data = pd.DataFrame(np.random.random((24, 5)), columns=['rain', 'sun', 'wind',
-                                                      ' grass', 'trees'])
+                                                        'grass', 'trees'])
 
 filename = './tmp.csv'
 data.to_csv(filename)
@@ -24,6 +24,8 @@ subprocess.run(['scp', filename,
                 'hpc-login.iter.org:/work/imas/shared/external/'
                 f'assets/nova/MD5/{secure_hash}'])
 
-source = intake.open_csv(f'http://static.iter.org/imas/assets/nova/MD5/{secure_hash}')
+source = intake.open_csv('http://static.iter.org/imas/assets/solps-iter/MD5/fadd1f3622808187793321c944886b91')
+source = intake.open_csv('http://static.iter.org/imas/assets/NOVA/MD5/07ec4f9d9e44fd9dcd7fd95cfd4951f9')
+
 
 source.discover()

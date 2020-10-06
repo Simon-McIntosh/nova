9c4b1eb5c50f853dd9012b5f4b249b2d64b8c5fc
Mcintosh Simon <mcintos@hpc-login02.iter.org>
2020-10-06 13:51:44 +0200

    pass bamboo working directory

--- docker/iter-ci.sh
+++ docker/iter-ci.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+wkdir=$1  # working directory
 . /usr/share/Modules/init/sh
 module use /work/imas/etc/modules/all
 # print hostname (debug)
@@ -9,7 +10,7 @@ module load udocker/1.1.4-intel-2020a-Python-2.7.18
 udocker --allow-root pull twistersi/nova:base
 udocker --allow-root create --name=nova_base twistersi/nova:base
 # run docker container (run pytest on nova/tests)
-udocker --allow-root run --volume=${bamboo.build.working.directory}:/nova nova_base -m pytest /nova/tests/ --junitxml=/nova/tests/results.xml
+udocker --allow-root run --volume=$wkdir:/nova nova_base -m pytest /nova/tests/ --junitxml=/nova/tests/results.xml
 # print docker containers
 udocker --allow-root ps
 

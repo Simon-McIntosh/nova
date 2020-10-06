c69856115261e1c84a7365edc3cf0a318e5ae656
Mcintosh Simon <mcintos@hpc-login02.iter.org>
2020-10-06 14:14:46 +0200

    docker try / except

--- docker/iter-ci.sh
+++ docker/iter-ci.sh
@@ -6,11 +6,17 @@ module use /work/imas/etc/modules/all
 hostname
 # load udocker
 module load udocker/1.1.4-intel-2020a-Python-2.7.18
-# pull and build (should use cached images here)
+docker_pytest="udocker --allow-root run --volume=$wkdir:/nova nova_base -m pytest /nova/tests/ --junitxml=/nova/tests/results.xml"
+{ # try
+# run from cached image
+$docker_pytest
+} || { # except
+# pull and build when cached image not found
 udocker --allow-root pull twistersi/nova:base
 udocker --allow-root create --name=nova_base twistersi/nova:base
 # run docker container (run pytest on nova/tests)
-udocker --allow-root run --volume=$wkdir:/nova nova_base -m pytest /nova/tests/ --junitxml=/nova/tests/results.xml
+$docker_pytest
+}
 # print docker containers
 udocker --allow-root ps
 

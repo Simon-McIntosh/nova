9a6920197afb0305d60f72b9c554d5543ba2db32
Mcintosh Simon <mcintos@hpc-login02.iter.org>
2020-10-06 13:40:07 +0200

    place CI build script under version control

--- docker/iter-ci.sh
+++ docker/iter-ci.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
-# initialize modules
 . /usr/share/Modules/init/sh
-module use /work/imas/etc/modules/all 
-# load latest GCC and udocker
-module load GCC 
+module use /work/imas/etc/modules/all
+# print hostname (debug)
+hostname
+# load udocker
 module load udocker/1.1.4-intel-2020a-Python-2.7.18
-# run docker container
+# pull and build (should use cached images here)
+udocker --allow-root pull twistersi/nova:base
+udocker --allow-root create --name=nova_base twistersi/nova:base
+# run docker container (run pytest on nova/tests)
+udocker --allow-root run --volume=${bamboo.build.working.directory}:/nova nova_base -m pytest /nova/tests/ --junitxml=/nova/tests/results.xml
+# print docker containers
 udocker --allow-root ps
-#udocker --allow-root create --name nova_base twistersi/nova:base
-#udocker --allow-root run --name nova twistersi/nova:base 
-
-#-v **/:/nova twistersi/nova:base -m pytest --junitxml=**/tests/results.xml
-# clearup
-#udocker rm nova
-
-# -o junit_suite_name=pytest
-#cat **/tests/results.xml
-
-# docker run --name twistersi/nova:base -v C:/Users/mcintos/Work/Code/nova:/nova nova -m pytest /nova/tests/
 

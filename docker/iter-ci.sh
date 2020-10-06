e0c7671f5088b5787803f6ff3b7d865ed7b664ab
Mcintosh Simon <mcintos@hpc-login02.iter.org>
2020-10-06 10:37:34 +0200

    iter-ci

--- docker/iter-ci.sh
+++ docker/iter-ci.sh
@@ -7,11 +7,13 @@ module use /work/imas/etc/modules/all
 module load Python/2.7.18-GCCcore-9.3.0
 module load udocker/1.1.4-intel-2020a-Python-2.7.18
 # run docker container
-udocker --allow-root run --name nova twistersi/nova:base 
+udocker --allow-root ps
+udocker --allow-root create --name nova_base twistersi/nova:base
+#udocker --allow-root run --name nova twistersi/nova:base 
 
 #-v **/:/nova twistersi/nova:base -m pytest --junitxml=**/tests/results.xml
 # clearup
-udocker rm nova
+#udocker rm nova
 
 # -o junit_suite_name=pytest
 #cat **/tests/results.xml

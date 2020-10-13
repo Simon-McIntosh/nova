dd0736db284708acf65ac151497a8376dc546642
Mcintosh Simon <mcintos@iter.org>
2020-10-13 12:07:27 +0200

    udocker ci debug

--- docker/iter-ci.sh
+++ docker/iter-ci.sh
@@ -10,10 +10,15 @@ module load udocker/1.1.4-intel-2020a-Python-2.7.18
 docker_pytest="udocker --allow-root run --volume=$wkdir:/nova nova_$label -m pytest /nova/tests/ --junitxml=/nova/tests/results.xml"
 { # try
 # run from cached image
+echo try from cached
 $docker_pytest
 } || { # except
 # pull and build when cached image not found
+echo listing local conatiners
+udocker --allow-root ps
+echo pulling twistersi/nova:$label
 udocker --allow-root pull twistersi/nova:$label
+echo creating image nova_$label
 udocker --allow-root create --name=nova_$label twistersi/nova:$label
 # run docker container (run pytest on nova/tests)
 $docker_pytest

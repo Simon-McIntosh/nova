744858b37e155310fb2af02c5f7a0e208df5ff40
Simon McIntosh <simon.mcintosh@iter.org>
2020-11-15 20:48:06 +0100

    minor

--- nova/development/plasma_coils.py
+++ nova/development/plasma_coils.py
@@ -15,6 +15,7 @@ ITER.separatrix = ITER.data['separatrix']
 
 ITER.current_update = 'feedback'
 ITER.Ic = 0
+ITER.Ip = -16.5e6
 
 #ITER.current_update = 'full'
 #ITER.Ic = 0
@@ -24,10 +25,10 @@ ITER.plasmagrid.optimize = 'newton'
 
 plt.set_aspect(0.8)
 
-zo = -0.5
-k = 5e6
+zo = 1
+k = 2e6
 
-for __ in range(50):
+for __ in range(25):
     err = (ITER.Opoint[1]-zo)
     ITER.update_separatrix(alpha=1)
 
@@ -36,9 +37,9 @@ for __ in range(50):
 
 ITER.plot(True, feedback=False)
 ITER.plot_null()
-#ITER.plasmagrid.plot_topology(True)
-ITER.plasmagrid.plot_flux(levels=101)
-
+ITER.plasmagrid.plot_topology(True)
+ITER.plasmagrid.plot_flux()
+plt.plot(*ITER.separatrix.boundary.xy)
 
 #ITER.plot_data(['firstwall', 'divertor'])
 #plt.plot(*ITER.data['divertor'].iloc[1:].values.T)

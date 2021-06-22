c1398752c747f8c90eba1735185ced0885115fbe
Simon Mcintosh <mcintos@iter.org>
2021-06-22 16:34:45 +0200

    v0 and v4 TFC displacments

--- nova/structural/TFC18.py
+++ nova/structural/TFC18.py
@@ -2,12 +2,32 @@
 from dataclasses import dataclass, field
 import os
 
+import numpy as np
+import numpy.typing as npt
 import pyvista as pv
+from rdp import rdp
 
 from nova.structural.ansyspost import AnsysPost
 from nova.structural.datadir import AnsysDataDir
 from nova.structural.plotter import Plotter
 from nova.structural.windingpack import WindingPack
+from nova.utilities.time import clock
+
+
+@dataclass
+class RamerDouglasPeucker:
+    """Subsample polyline using RDP algorithum."""
+
+    points: npt.ArrayLike
+    epsilon: 0.01
+
+    def __post_init__(self):
+        """Apply RDP algorithum."""
+        self.samples = np.array(rdp(self.points, self.epsilon))
+
+    def __len__(self):
+        """Return sample lenght."""
+        return len(self.samples)
 
 
 @dataclass
@@ -39,8 +59,6 @@ class TFC18(AnsysDataDir, Plotter):
         except FileNotFoundError:
             self.load_ansys()
             self.load_mesh()
-            self.interpolate()
-            self.mesh.save(self.vtk_file)
 
     def load_ansys(self):
         """Load ansys vtk mesh."""
@@ -54,20 +72,55 @@ class TFC18(AnsysDataDir, Plotter):
         """Load referance windingpack ccl."""
         self.mesh = WindingPack('TFC1_CL').mesh
         self.mesh.clear_point_arrays()
+        self.mesh = self.interpolate_coils(self.mesh, self.ansys)
+        mesh = self.mesh.copy()
+        self.mesh.clear_arrays()
+        for scn in self.scenario:
+            self.mesh[scn] = mesh[scn]
+        self.mesh.save(self.vtk_file)
 
-    def interpolate(self):
-        """Interpolate ansys displacements to winding pack centrelines."""
-        self.mesh = self.mesh.interpolate(
-            self.ansys, sharpness=2, radius=1.0, strategy='closest_point')
+    def interpolate_coils(self, source, target, sharpness=3, radius=1.5,
+                          n_cells=7):
+        """Retun mesh interpolant."""
+        mesh = pv.PolyData()
+        n_coils = source.n_cells // n_cells
+        tick = clock(n_coils, header='Interpolating Ansys displacements.')
+        for n_coil in range(n_coils):
+            mesh += source.extract_cells(
+                range(n_cells*n_coil, n_cells*(n_coil+1))).interpolate(
+                target, sharpness=sharpness, radius=radius,
+                strategy='closest_point')
+            tick.tock()
+        return mesh
+
+    def compress(self, epsilon=5e-4):
+        """Compress vtk mesh using rdp algorithum."""
+        mesh = pv.PolyData()
+        n_cells = self.mesh.n_cells
+        tick = clock(n_cells, header='Compressing vtk mesh.')
+        for cell in range(n_cells):
+            cell = self.mesh.extract_cells(cell)
+            fit = RamerDouglasPeucker(cell.points, epsilon)
+            submesh = pv.Spline(fit.samples)
+            submesh.clear_point_arrays()
+            mesh += submesh
+            tick.tock()
+        self.mesh = self.interpolate_coils(mesh, self.mesh)
 
     def plot(self):
         """Plot warped shape."""
-        self.warp('TFonly')
+        self.warp('TFonly-cooldown')
+
+    def animate(self):
+        filename = os.path.join(self.directory, self.file)
+        super().animate(filename, 'TFonly', view='xy')
+
+
 
 
 if __name__ == '__main__':
 
-    tf = TFC18('v4', 'WP', scenario={'deadweight': 0, 'cooldown': 1,
-                                     'TFonly': 2})
-
+    tf = TFC18('v0', 'WP', scenario={'cooldown': 1, 'TFonly': 2})
+    tf.mesh['TFonly-cooldown'] = tf.mesh['TFonly'] - tf.mesh['cooldown']
     tf.plot()
+    #tf.animate()
--- nova/structural/plotter.py
+++ nova/structural/plotter.py
@@ -1,18 +1,53 @@
 """Custom pyvista plotting methods."""
+import numpy as np
 import pyvista as pv
+
+from nova.utilities.time import clock
 
 
 class Plotter:
     """Custom pyvista plotting methods."""
 
-    def warp(self, scalars: str, factor=75, opacity=0.5):
+    def warp(self, scalars: str, factor=75, opacity=0.5, plotter=None):
         """Plot warped with mesh."""
-        if factor == 0:
-            return self.mesh.plot(scalars=scalars)
         plotter = pv.Plotter()
         if opacity > 0:
             plotter.add_mesh(self.mesh, scalars=None, color='w',
-                             opacity=opacity)
+                             opacity=opacity, smooth_shading=True)
         warp = self.mesh.warp_by_vector(scalars, factor=factor)
-        plotter.add_mesh(warp, scalars=scalars)
+        plotter.add_mesh(warp, scalars=scalars, smooth_shading=True)
         plotter.show()
+        return plotter
+
+    def animate(self, filename: str, scalars: str, max_factor=100, frames=31,
+                view='iso'):
+        """Animate warped displacments."""
+        plotter = pv.Plotter(notebook=False, off_screen=True,
+                             window_size=[400, 400], multi_samples=8,
+                             line_smoothing=True)
+
+        reference = self.mesh.copy()
+        plotter.add_mesh(reference, scalars=None, color='w', opacity=0.5,
+                         smooth_shading=True)
+        plotter.add_mesh(self.mesh, scalars=scalars, smooth_shading=True)
+        plotter.camera.zoom(1.5)
+        if view != 'iso':
+            getattr(plotter, f'view_{view}')()
+            plotter.camera.zoom(5)
+        filename += f'_{view}'
+        plotter.open_gif(f'{filename}.gif')
+
+
+        # Update Z and write a frame for each updated position
+        factors = np.linspace(0, max_factor, frames // 2)
+        factors = np.append(factors, factors[::-1][1:])
+        tick = clock(len(factors), header='Generating displacement gif.')
+        for factor in factors:
+            warp = self.mesh.warp_by_vector(scalars, factor=factor)
+            plotter.update_coordinates(warp.points, render=False)
+            # plotter.update_scalars(warp[scalars], render=False)
+            plotter.mesh.compute_normals(cell_normals=False, inplace=True)
+            plotter.render()
+            plotter.write_frame()
+            tick.tock()
+        plotter.close()

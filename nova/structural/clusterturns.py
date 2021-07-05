ffae461de928a86e8a1b3a1239705133ed78b387
Mcintosh Simon <mcintos@iter.org>
2021-07-05 22:50:47 +0200

    TFC18 cluster turns

--- nova/structural/clusterturns.py
+++ nova/structural/clusterturns.py
@@ -1,6 +1,7 @@
 """Manage as-designed coil winding pack descriptors."""
 from dataclasses import dataclass, field
 
+import numpy as np
 import numpy.typing as npt
 import pyvista as pv
 import sklearn.cluster
@@ -20,18 +21,24 @@ class ClusterTurns:
 
     def __post_init__(self):
         """Identify clusters and build mesh."""
-        self.extract()
-        self.build()
+        self.update(self.n_clusters)
 
-    def extract(self):
+    def update(self, n_clusters):
+        """Update turn clustering."""
+        self.n_clusters = n_clusters
+        self.extract_clusters()
+        self.build_mesh()
+
+    def extract_clusters(self):
         """Extract clusters from low-field mid-plane section."""
         n_turns = self.ccl_mesh.n_cells // 18
         self.turns = self.ccl_mesh['turns'][:n_turns]
+
         self.clusters = sklearn.cluster.KMeans(
             n_clusters=self.n_clusters,
-            random_state=0).fit_predict(self.turns)
+            random_state=6, n_init=20, tol=1e-4).fit_predict(self.turns)
 
-    def build(self):
+    def build_mesh(self):
         """Generate reduced ordered mesh from clustered turn data."""
         self.mesh = pv.PolyData()
         for i in range(18):
@@ -41,20 +48,52 @@ class ClusterTurns:
                 index = self.clusters == i
                 self.mesh += pv.Spline(points[index].mean(axis=0))
 
-    def plot(self):
+    def plot_slice(self, axes=None, ms=8):
         """Plot clusters."""
+        if axes is None:
+            axes = plt.subplots(1, 1)[1]
         for i in range(self.n_clusters):
             index = self.clusters == i
-            plt.plot(self.turns[index, 0], self.turns[index, 1], 'o', ms=15)
-            plt.axis('equal')
-            plt.axis('off')
+            color = f'C{i%10}'
+            axes.plot(self.turns[index, 0], self.turns[index, 1],
+                      's', color=color, ms=ms, alpha=0.5)
+            axes.plot(self.turns[index, 0], self.turns[index, 1],
+                      'o', ms=0.7*ms, color=color, alpha=0.5)
+            axes.plot(self.turns[index, 0].mean(),
+                      self.turns[index, 1].mean(), '.', color='gray')
+            axes.axis('equal')
+            axes.axis('off')
+        axes.text(self.turns[:, 0].mean(), self.turns[:, 1].max(),
+                  f'{self.n_clusters}', va='bottom')
 
+    def plot_slice_array(self, clusters=[1, 3, 5, 9]):
+        """Plot low-filed mid-plane slices illustrating cluster options."""
+        axes = plt.subplots(1, len(clusters))[1]
+        for i, n_cluster in enumerate(clusters):
+            self.update(n_cluster)
+            self.plot_slice(axes[i])
+
+    def plot_turn_array(self, clusters=[1, 3, 6, 11], nrows=2):
+        """Plot reduced order turns."""
+        ncols = int(np.ceil(len(clusters) / nrows))
+        plotter = pv.Plotter(shape=(nrows, ncols))
+        for i, n_cluster in enumerate(clusters):
+            plotter.subplot(i//nrows, i%ncols)
+            self.update(n_cluster)
+            plotter.add_mesh(self.mesh)
+        plotter.link_views()
+        plotter.show()
 
 if __name__ == '__main__':
 
     ccl = UniformWindingPack()
-    cluster = ClusterTurns(ccl.mesh, 5)
-    cluster.plot()
+    cluster = ClusterTurns(ccl.mesh, 8)
+
+    cluster.update(3)
+    #cluster.plot_slice_array()
+
+    cluster.plot_turn_array()
+
 
 
     '''

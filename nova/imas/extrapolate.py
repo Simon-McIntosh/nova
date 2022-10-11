bfcffe72ae1b0068ff36dc536c8d9eb80dea031b
Simon McIntosh <simon.mcintosh@iter.org>
2022-10-11 13:35:03 +0200

    extrapolate imas minor

--- nova/imas/extrapolate.py
+++ nova/imas/extrapolate.py
@@ -194,12 +194,12 @@ class Extrapolate(BiotPlot, Machine, Grid, IDS):
 
 if __name__ == '__main__':
 
-    pulse, run = 114101, 41  # JINTRAC
+    #pulse, run = 114101, 41  # JINTRAC
     #  pulse, run = 130506, 403  # CORSICA
 
     database = Database(pulse, run, 'equilibrium', machine='iter')
     coilset = Extrapolate(ids_data=database.ids_data,
-                          dplasma=-1000, number=2000)
+                          dplasma=-2000, number=2000)
     #coilset.build()
-    #coilset.ionize(0)
-    #coilset.plot('psi')
+    coilset.ionize(0)
+    coilset.plot('psi')

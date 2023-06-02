fede8985142a23f62a6d1c552eff124e763ca061
Simon McIntosh <simon.mcintosh@iter.org>
2023-06-02 17:23:27 +0200

    fixup unused imports

--- apps/pulsedesign/main.py
+++ apps/pulsedesign/main.py
@@ -5,12 +5,9 @@ from bokeh.io import curdoc
 from bokeh.layouts import column
 from bokeh.models import (
     AutocompleteInput,
-    CheckboxButtonGroup,
     NumericInput,
     Select,
     Slider,
-    TabPanel,
-    Tabs,
     TextInput,
 )
 from bokeh.plotting import figure
@@ -58,26 +55,11 @@ plasma = poloidal.multi_polygons(
     "x", "z", fill_alpha="ionize", line_alpha=0, source=source["plasma"]
 )
 
-# coils = poloidal.multi_polygons(source=source['coil'])
-
-
-poloidal_toggle = CheckboxButtonGroup(
-    labels=["plasma", "wall", "nulls", "flux", "coils"],
-    active=[0, 1, 2, 3],
-    name="poloidal_toggle",
-)
-
 pprime = figure(height=150)
 pprime.line("psi_norm", "dpressure_dpsi", source=source["profiles"])
 
 ffprime = figure(height=150)
 ffprime.line("psi_norm", "f_df_dpsi", source=source["profiles"])
-
-
-# plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-# kwargs = Properties().patch_properties(simulator.frame.part, simulator.frame.area)
-# plot.multi_polygons(**simulator.subframe.polygeo.polygons)
-
 
 itime = Slider(
     title="itime",
@@ -99,30 +81,8 @@ def update_itime(attr, old, new):
     simulator.itime = new
 
 
-def poloidal_visibility(attr, old, new):
-    """Update wall visibility."""
-    if old == new:
-        return
-    for i, label in enumerate(poloidal_toggle.labels):
-        setattr(label, "visible", i in new)
-
-
 simulator.itime = 0
-
 itime.on_change("value", update_itime)
 
-# ids.on_change('')
-
-# poloidal_toggle.on_change('active', poloidal_visibility)
-
-ids_tab = TabPanel(child=ids, title="IDS")
-profile_tab = TabPanel(child=sliders, title="Profile")
-
-
-user_input = Tabs(tabs=[ids_tab, profile_tab], name="user_input")
-
-
-# Set up layouts and add to document
-curdoc().add_root(user_input)
+curdoc().add_root(sliders)
 curdoc().add_root(poloidal)
-curdoc().add_root(poloidal_toggle)

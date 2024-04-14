c79bfaf08d3c8a1b5b43f34a4067e2e233677f76
Simon McIntosh <simon.mcintosh@iter.org>
2024-04-14 22:05:54 +0200

    al-python poetry automatic build

--- build.py
+++ build.py
@@ -1,109 +1,89 @@
 """Build IMAS Access Layer."""
 
 import os
-import platform
-import re
-import subprocess
-import sys
-import sysconfig
-from distutils.version import LooseVersion
-from typing import Any, Dict
-
+from pathlib import Path
+from setuptools import Distribution, Extension
 from setuptools.command.build_ext import build_ext
-from setuptools.extension import Extension
+
+import shutil
+
+import subprocess
 
 
 class CMakeExtension(Extension):
-    """Extend Extension with Cmake sourcedir."""
-
-    name: str
+    """Extend setuptools Extension to build cmake source."""
 
     def __init__(self, name: str, sourcedir: str = "") -> None:
         super().__init__(name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
+        self.sourcedir = str(Path(sourcedir).resolve())
 
 
-class ExtensionBuilder(build_ext):
-    """Extend build_ext."""
+class BuildExt(build_ext):
+    """Build IMAS al-python extension."""
 
-    def run(self) -> None:
-        """Validate cmake."""
-        self.validate_cmake()
-        super().run()
+    inplace: bool = True
 
-    def build_extension(self, ext: Extension) -> None:
-        """Build cmake extension."""
-        if isinstance(ext, CMakeExtension):
-            self.build_cmake_extension(ext)
-        else:
-            super().build_extension(ext)
+    @property
+    def cfg(self):
+        """Return cfg attribute."""
+        if self.debug:
+            return "Debug"
+        return "Release"
 
-    def validate_cmake(self) -> None:
-        """Validate cmake extension."""
-        cmake_extensions = [x for x in self.extensions if isinstance(x, CMakeExtension)]
-        if len(cmake_extensions) > 0:
-            try:
-                out = subprocess.check_output(["cmake", "--version"])
-            except OSError:
-                raise RuntimeError(
-                    "CMake must be installed to build the following extensions: "
-                    + ", ".join(e.name for e in cmake_extensions)
-                )
-            if platform.system() == "Windows":
-                cmake_version = LooseVersion(
-                    re.search(r"version\s*([\d.]+)", out.decode()).group(1)
-                )  # type: ignore
-                if cmake_version < "3.1.0":
-                    raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-    def build_cmake_extension(self, ext: CMakeExtension) -> None:
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-            # "-DPYTHON_EXECUTABLE=" + sys.executable,
-        ]
-
-        cfg = "Debug" if self.debug else "Release"
-        build_args = ["--config", cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += [
-                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
-            ]
-            if sys.maxsize > 2**32:
-                cmake_args += ["-A", "x64"]
-            build_args += ["--", "/m"]
-        else:
-            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-            build_args += ["--", "-j4"]
-        cmake_args += ["-DPYTHON_INCLUDE_DIR={}".format(sysconfig.get_path("include"))]
-        # cmake_args += ["-DCMAKE_INSTALL_PREFIX={}".format()]
+    def build_extension(self, ext):
+        """Overwrite build_ext.build_extension to cmake al-python."""
         env = os.environ.copy()
-        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get("CXXFLAGS", ""), self.distribution.get_version()
-        )
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
+        ext_full_path = self.get_ext_fullpath(ext.name)
+        self.build_lib = Path(ext_full_path).parent.resolve()
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={self.build_lib}",
+            f"-DCMAKE_BUILD_TYPE={self.cfg}",
+        ]
+        build_args = ["--config", self.cfg, "--", "-j4"]
+        Path(self.build_temp).mkdir(parents=True, exist_ok=True)
         subprocess.check_call(
             ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
         )
         subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_temp, env=env
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
         )
 
+    def copy(self):
+        """Copy build_temp to build/imas."""
+        imas_temp = Path(self.build_temp) / "imas"
+        imas_module = Path(self.build_temp).parent.parent / "imas"
+        if imas_module.is_dir():
+            shutil.rmtree(imas_module)
+        shutil.copytree(imas_temp, imas_module)
+        imas_lib = imas_module / "lib"
+        if imas_lib.is_dir():
+            shutil.rmtree(imas_lib)
+        shutil.copytree(self.build_lib, imas_lib)
 
-def build(setup_kwargs: Dict[str, Any]) -> None:
-    """Build IMAS al-python."""
-    ext_modules = [
-        CMakeExtension(
-            "al_python",
-            sourcedir="../al-python",
-        )
-    ]
-    setup_kwargs.update(
-        {
-            "ext_modules": ext_modules,
-            "cmdclass": dict(build_ext=ExtensionBuilder),
-            "zip_safe": False,
-        }
-    )
+
+def build():
+    """Build IMAS python access layer."""
+    ext_modules = [CMakeExtension("imas", sourcedir="../al-python")]
+
+    distribution = Distribution({"ext_modules": ext_modules})
+    # distribution.package_dir = {"": "extended"}
+    cmd = BuildExt(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+    cmd.copy()
+
+    """
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        print("output", output)
+        print("re", relative_extension)
+        print(os.listdir())
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+    """
+
+
+if __name__ == "__main__":
+    build()
